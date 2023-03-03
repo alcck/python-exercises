aedad406a08c7c7a90f487740c16bab033bf93ff
alcck <alcck2000@gmail.com>
2023-03-03 20:38:22 +0300

    creating interatcive pages with thinkter

--- tkninter.py
+++ tkninter.py
@@ -1,35 +1,114 @@
 import tkinter
 from tkinter import *
 from functools import partial
+from tkinter import messagebox
 
 win = Tk()
-l1 = Label(win,text="First Number")
-l1.grid(row=1,column=0)
 
-l2 = Label(win,text="Second Number")
-l2.grid(row=2,column=0)
+def hello():
+    messagebox.showinfo('from computer','hey there!')
+b = Button(win,text = 'click pls',command = hello)
+b.pack()
 
-label = Label(win)
-label.grid(row=6,column=2)
 
-x1 = StringVar()
-x2 = StringVar()
 
-e1 = Entry(win,textvariable=x1)
-e1.grid(row=1,column=2)
-e2 = Entry(win,textvariable=x2)
-e2.grid(row=2,column=2)
 
-def sum(label,x1,x2):
-    n1 = (x1.get())
-    n2 = (x2.get())
-    sum = int(n1) + int(n2)
-    label.config(text='sum is :%d'%sum)
-    return
 
-sum= partial(sum,label,x1,x2)
-button = Button(win,text = "Calculate", command=sum)
-button.grid(row=3,column=0)
+
+
+
+#lb = Listbox(win)
+#lb.insert(1,'python')
+#lb.insert(2,'c')
+#lb.insert(3,'c++')
+#lb.insert(4,'jquery')
+#lb.insert(5,'ruby')
+
+#lb.pack()
+
+
+
+
+
+
+
+
+
+
+#frame = Frame(win)
+#frame.pack()
+
+#frame2 = Frame(win)
+#frame2.pack(side=BOTTOM)
+
+#rb = Button(frame,text='Red',fg='red')
+#rb.pack(side=RIGHT)
+
+#bb = Button(frame,text='Blue',fg='blue')
+#bb.pack(side=TOP)
+
+#blb = Button(frame,text='Black',fg='black')
+#blb.pack(side=BOTTOM)
+
+#gb = Button(frame,text='Green',fg='green')
+#gb.pack(side=LEFT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#l1 = Label(win,text="First Number")
+#l1.grid(row=1,column=0)
+
+#l2 = Label(win,text="Second Number")
+#l2.grid(row=2,column=0)
+
+#label = Label(win)
+#label.grid(row=6,column=2)
+
+#x1 = StringVar()
+#x2 = StringVar()
+
+#e1 = Entry(win,textvariable=x1)
+#e1.grid(row=1,column=2)
+#e2 = Entry(win,textvariable=x2)
+#e2.grid(row=2,column=2)
+
+#def sum(label,x1,x2):
+    #n1 = (x1.get())
+    #n2 = (x2.get())
+    #sum = int(n1) + int(n2)
+    #label.config(text='sum is :%d'%sum)
+    #return
+
+#sum= partial(sum,label,x1,x2)
+#button = Button(win,text = "Calculate", command=sum)
+#button.grid(row=3,column=0)
+
+
+
+
+
+
+
+
 
 
 #l = Label(win,text='username')
@@ -60,6 +139,16 @@ button.grid(row=3,column=0)
 
 #r3 = Radiobutton(win, text='option1',variable=var,value=3)
 #r3.pack()
+
+
+
+
+
+
+
+
+
+
 
 #win.geometry("800x600")
 
