c190db86721c3ee9ba31e37eb2490fcba4aff29b
alcck <alcck2000@gmail.com>
2023-03-06 02:01:24 +0300

    creating a database

--- db.py
+++ db.py
@@ -1,3 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('lite.db')
+def createtable():
+    conn = sqlite3.connect('lite.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE data(rollno INTEGER, name TEXT, marks REAL")
+    conn.commit()
+    conn.close()
+
+createtable()
