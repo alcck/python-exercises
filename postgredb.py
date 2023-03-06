279257bf8c788b2cad8e0a230c792fba3ea6c896
alcck <alcck2000@gmail.com>
2023-03-07 00:03:37 +0300

    creating a database using sqlite3

--- postgredb.py
+++ postgredb.py
@@ -14,5 +14,37 @@ def insert(roll, nam, mark):
     conn.commit()
     conn.close()
 
-insert(1,'capybara',100)
-insert(2,'duck',150)
+#insert(1,'capybara',100)
+#insert(2,'duck',150)
+
+def view():
+    conn = psycopg2.connect("dbname='data2' user='postgres' password='nimda123*' port='5432' host='localhost' ")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM data2")
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+#print(view())
+
+def delete(roll):
+    conn = psycopg2.connect("dbname='data2' user='postgres' password='nimda123*' port='5432' host='localhost' ")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM data2 WHERE rollno=%s", (roll,))
+    conn.commit()
+    conn.close()
+
+
+#delete(2)
+#print(view())
+
+def update(nam, mark, roll):
+    conn = psycopg2.connect("dbname='data2' user='postgres' password='nimda123*' port='5432' host='localhost' ")
+    cur = conn.cursor()
+    cur.execute("UPDATE data2 SET name=%s , marks=%s  WHERE rollno=%s", (nam, mark, roll))
+    conn.commit()
+    conn.close()
+
+update('mouse', 200, 2)
+print(view())
--- sqlite3db.py
+++ sqlite3db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def createtable():
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
@@ -7,14 +8,47 @@ def createtable():
     conn.commit()
     conn.close()
 
-#createtable()
+
+# createtable()
 
 def insert(roll, nam, mark):
-    conn = sqlite3.connect("lite.db ")
+    conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO data VALUES(?,?,?)",(roll,nam,mark))
+    cur.execute("INSERT INTO data VALUES(?,?,?)", (roll, nam, mark))
     conn.commit()
     conn.close()
 
-insert(1,'capybara',100)
-insert(2,'duck',150)
+
+# insert(1,'capybara',100)
+# insert(2,'duck',150)
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM data")
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+
+def delete(roll):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM data WHERE rollno=?", (roll,))
+    conn.commit()
+    conn.close()
+
+
+# delete()
+
+def update(nam, mark, roll):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE data SET name=? , marks=?  WHERE rollno=?", (nam, mark, roll))
+    conn.commit()
+    conn.close()
+
+
+update('mouse', 200, 2)
+print(view())
